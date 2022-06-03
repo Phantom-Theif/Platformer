1c4254ea6b94970de0551ca0fa334a77a50b7231
Phantom-Theif <94059203+Phantom-Theif@users.noreply.github.com>
2022-06-03 10:02:04 -0400

    Update One Cheek.py

--- One Cheek.py
+++ One Cheek.py
@@ -16,6 +16,7 @@ pygame.display.set_caption('Platformer')
 #define game variables
 tile_size = 50
 
+game_over = 0
 
 #load images
 bg_img = pygame.image.load('Factory.png')
@@ -27,6 +28,7 @@ bg_img = pygame.image.load('Factory.png')
 
 
 class Player():
+    global game_over
     def __init__(self, x, y):
         self.images_right = []
         self.images_left = []
@@ -37,6 +39,7 @@ class Player():
             self.images_right.append(img_right)
             img_left = pygame.transform.flip(img_right, True, False)
             self.images_left.append(img_left)
+        self.dead_image = pygame.image.load("ghost.png")
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -46,73 +49,80 @@ class Player():
         self.vel_y = 0
         self.jumped = False
         self.direction = 0
-    def update(self):
+    def update(self, game_over):
         dx = 0
         dy = 0
         walk_cooldown = 5
 
-        #get keypresses
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE] and self.jumped == False:
-            self.vel_y = -16
-            self.jumped = True
-        if key[pygame.K_SPACE] == False:
-            self.jumped = False
-        if key[pygame.K_LEFT]:
-            dx -= 5
-            self.counter += 1
-            self.direction = -1
-        if key[pygame.K_RIGHT]:
-            dx += 5
-            self.counter += 1
-            self.direction = 1
-        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
-            self.counter = 0
-            self.index = 0
-            self.image = self.images_right[self.index]
-        #handle animation
-        if self.counter > walk_cooldown:
-            self.counter = 0 
-            self.index += 1
-            if self.index >= len(self.images_right):
+        if game_over == 0:
+            #get keypresses
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False:
+                self.vel_y = -16
+                self.jumped = True
+            if key[pygame.K_SPACE] == False:
+                self.jumped = False
+            if key[pygame.K_LEFT]:
+                dx -= 5
+                self.counter += 1
+                self.direction = -1
+            if key[pygame.K_RIGHT]:
+                dx += 5
+                self.counter += 1
+                self.direction = 1
+            if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+                self.counter = 0
                 self.index = 0
-            if self.direction == 1:
                 self.image = self.images_right[self.index]
-            if self.direction == -1:
-                self.image = self.images_left[self.index]
-            
-        #add gravity
-        self.vel_y += 1
-        if self.vel_y > 10:
-            self.vel_y = 10
-        dy += self.vel_y
+            #handle animation
+            if self.counter > walk_cooldown:
+                self.counter = 0 
+                self.index += 1
+                if self.index >= len(self.images_right):
+                    self.index = 0
+                if self.direction == 1:
+                    self.image = self.images_right[self.index]
+                if self.direction == -1:
+                    self.image = self.images_left[self.index]
+                
+            #add gravity
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
 
-        #check for collision
-        for tile in world.tile_list:
-            #check for direction in the x direction
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-            #check for collision in y direction. 
-            
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                #check if below the ground i.e. jumping
-                if self.vel_y < 0:
-                    dy = tile[1].bottom - self.rect.top
-                if self.vel_y >= 0:
-                    dy = tile[1].top - self.rect.bottom
+            #check for collision
+            for tile in world.tile_list:
+                #check for direction in the x direction
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                #check for collision in y direction. 
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    #check if below the ground i.e. jumping
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
 
+                    #check for collision with enemies.
 
-        #update play coordinates
-        self.rect.x += dx
-        self.rect.y += dy
+                if pygame.sprite.spritecollide(self, traumaflame_group, False):
+                    game_over = -1
+                if pygame.sprite.spritecollide(self, traumaflame_group, False):
+                    game_over = -1
 
-        if self.rect.bottom > screen_height:
-            self.rect.bottom = screen_height
-            dy = 0
-        #draw okayer onto screen
+            #update play coordinates
+            self.rect.x += dx
+            self.rect.y += dy
+        
+
+        #draw player onto screen
         screen.blit(self.image, self.rect)
         # pygame.draw.rect(screen, (255, 0, 0), self.rect, 2)
 
+        return game_over
 
 class World():
     def __init__(self, data):
@@ -180,7 +190,7 @@ world_data = [
 [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
-[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 3, 0, 3, 0, 0, 2, 0, 1],
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 3, 0, 0, 0, 0, 2, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -207,10 +217,11 @@ while run:
     
     world.draw()
 
-    traumaflame_group.update()
+    if game_over == 0:
+        traumaflame_group.update()
     traumaflame_group.draw(screen)
 
-    player.update()
+    game_over = player.update(game_over)
     #draw_grid()
 
     for event in pygame.event.get():
